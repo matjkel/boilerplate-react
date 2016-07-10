b036b9c3ce56a6ec40363fd4201b7972be63a98b
Rico Rojas <djizco@yahoo.com>
2016-07-10 05:13:23 -0500

    Use browserHistory only with real server

--- client/routes.js
+++ client/routes.js
@@ -3,10 +3,17 @@ import Main        from './containers/Main.js';
 import HomePage    from './containers/HomePage.js';
 import CounterPage from './containers/CounterPage.js';
 
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute } from 'react-router';
+
+let historyType;
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development'){
+  historyType = require('react-router').browserHistory;
+} else {
+  historyType = require('react-router').hashHistory;
+}
 
 export default (
-  <Router history={browserHistory}>
+  <Router history={historyType}>
     <Route path='/' component={Main}>
       <IndexRoute component={HomePage} />
       <Route path='/counter' component={CounterPage}/>
